cd0a927b3c17744ddc26d2fa6bec2d39dc40ef50
Shubham Nigam <shubham.nigam@newvision-software.com>
2026-06-12 11:21:18 +0530

    Test

--- src/custom_fixture/fixture.ts
+++ src/custom_fixture/fixture.ts
@@ -1,27 +1,27 @@
 import { test as base, expect } from "@playwright/test";
-import { loginAction } from "../actions/loginAction/loginAction";
+import { loginAction } from "../actions/loginAction/loginAction.ts";
 import loginData from "../testdata/login.json";
-
+ 
 type appActions = {
     login: loginAction;
 };
-
+ 
 type Fixtures = {
     appAction: appActions;
 };
-
+ 
 export const test = base.extend<Fixtures>({
     appAction: async ({ page }, use) => {
-        
+       
         // Navigate BEFORE using actions
         await page.goto(loginData.BaseURL);
-
+ 
         const appAction: appActions = {
             login: new loginAction(page)
         };
-
+ 
         await use(appAction);
     }
 });
-
+ 
 export { expect };
