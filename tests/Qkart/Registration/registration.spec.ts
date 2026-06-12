25b5a78c23a3eb867e1b514ee4fa28341890a1d8
LALIT1804-NEWVISION <lalit.gupta@newvision-software.com>
2026-06-12 19:16:17 +0530

    commit

--- tests/Qkart/Registration/registration.spec.ts
+++ tests/Qkart/Registration/registration.spec.ts
@@ -1,19 +1,17 @@
 import { test } from "../../../src/custom_fixture/fixture";
 
-test.describe("SC_001_Registration Functionality Validation", () => {
   test("REG_001 - Verify Registration with Valid Details", async ({appAction}) => {
     await appAction.register.Click_On_RegisterLink();
     await appAction.register.registeration_details();
     await appAction.register.verifyRegistrationSuccessMessage();
   });
+  
   test("REG_002 - Verify Registration with Existing Username", async ({appAction}) => {
     await appAction.register.Click_On_RegisterLink();
     await appAction.register.registerWithExistingUsername();
     await appAction.register.verifyErrorMessage("Username already exists");
   });
-});
 
-test.describe("SC_002_Mandatory Field Validation", () => {
   test("REG_003 - Verify Error Message for Blank Username Field", async ({appAction}) => {
     await appAction.register.Click_On_RegisterLink();
     await appAction.register.registerWithBlankUsername();
@@ -31,9 +29,7 @@ test.describe("SC_002_Mandatory Field Validation", () => {
     await appAction.register.registerWithAllBlankFields();
     await appAction.register.verifyErrorMessage("Username is a required field");
   });
-});
 
-test.describe("SC_003_Field Length Validation", () => {
   test("REG_006 - Verify Username Minimum Length Validation", async ({appAction}) => {
     await appAction.register.Click_On_RegisterLink();
     await appAction.register.registerWithShortUsername();
@@ -45,12 +41,10 @@ test.describe("SC_003_Field Length Validation", () => {
     await appAction.register.registerWithShortPassword();
     await appAction.register.verifyErrorMessage("Password must be at least 6 characters");
   });
-});
 
-test.describe("SC_004_Password Validation", () => {
   test("REG_008 - Verify Password and Confirm Password Mismatch", async ({appAction}) => {
     await appAction.register.Click_On_RegisterLink();
     await appAction.register.registerWithPasswordMismatch();
     await appAction.register.verifyErrorMessage("Passwords do not match");
   }); 
-});
+
