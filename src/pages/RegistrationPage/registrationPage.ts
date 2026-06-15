0e90c0b7bf704f7f7f8a47b460ecdc6b291b8b85
Shubham Nigam <shubham.nigam@newvision-software.com>
2026-06-15 12:55:05 +0530

    changes

--- src/pages/RegistrationPage/registrationPage.ts
+++ src/pages/RegistrationPage/registrationPage.ts
@@ -7,7 +7,9 @@ export class RegisterationPage {
   readonly password: Locator;
   readonly confirmPassword: Locator;
   readonly registerNowButton: Locator;
+  readonly errorMessage: Locator;
   readonly successMessage: Locator;
+ 
 
   constructor(page: Page) {
     this.page = page;
@@ -15,7 +17,8 @@ export class RegisterationPage {
     this.Username = page.getByPlaceholder('Enter Username');
     this.password = page.getByPlaceholder('Enter a password with minimum 6 characters');
     this.confirmPassword = page.getByPlaceholder('Re-enter your password to confirm');
-    this.registerNowButton = page.locator('#register-button');
-    this.successMessage = page.locator('.result');
+    this.registerNowButton = page.getByRole('button', { name: 'Register Now' });
+    this.errorMessage = page.locator('//div[@id="notistack-snackbar"]');
+    this.successMessage = page.locator('//div[@id="notistack-snackbar"]');
   }
 }
