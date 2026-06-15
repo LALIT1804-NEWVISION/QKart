3d31c80d443d783b0a83d828f7f135c956ba3d0d
deepanshusahu127 <deepanshu.sahu@newvision-software.com>
2026-06-15 13:58:55 +0530

    d

--- src/custom_fixture/fixture.ts
+++ src/custom_fixture/fixture.ts
@@ -1,34 +1,28 @@
-import { test as base, expect, Page } from '@playwright/test';
-import { RegisterationAction } from '../actions/RegistrationAction/registrationAction';
-import RegisterationData from "../testdata/registrationData.json";
-
-type AppActions = {
-    register: RegisterationAction;
-
+import { test as base, expect } from "@playwright/test";
+import { loginAction } from "../actions/loginAction/loginAction";
+import loginData from "../testdata/login.json";
+ 
+type appActions = {
+    login: loginAction;
 };
-
+ 
 type Fixtures = {
-    gotoBaseUrl: void;
-    appAction: AppActions;
+    appAction: appActions;
 };
-
+ 
 export const test = base.extend<Fixtures>({
-    gotoBaseUrl: [
-        async ({ page }, use) => {
-            await page.goto(RegisterationData.baseUrl);
-            await expect(page).toHaveURL(RegisterationData.baseUrl);
-            await use();
-        },
-        { auto: true },
-    ],
-
     appAction: async ({ page }, use) => {
-        const appAction: AppActions = {
-            register: new RegisterationAction(page), 
+       
+        // Navigate BEFORE using actions
+        await page.goto(loginData.BaseURL);
+ 
+        const appAction: appActions = {
+            login: new loginAction(page)
         };
+ 
         await use(appAction);
-    },
+    }
 });
-
-export { expect } from "@playwright/test";
-
+ 
+export { expect };
+ 
