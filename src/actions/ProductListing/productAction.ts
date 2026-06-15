90bbd377809e6cbc6518bc6c5a1f670d9c7733be
Aamna <aamna.zia@newvision-software.com>
2026-06-15 18:23:48 +0530

    Added TC-001, 003 - product listing

--- src/actions/ProductListing/productAction.ts
+++ src/actions/ProductListing/productAction.ts
@@ -3,11 +3,34 @@ import {ProductPage} from "../../pages/ProductListingPage/productPage";
 
 
 export class ProductAction {
-    readonly ProductAction_instance: ProductAction;
+    readonly productPage: ProductPage;
 
     constructor(page: Page) {
-        this.ProductAction_instance = new ProductAction(page);
+        this.productPage = new ProductPage(page);
     }
 
+    async verifyPageLoadState() {
+        await this.productPage.page.waitForLoadState("load");
+        await expect(this.productPage.homePageLogo).toBeVisible();
+    }
 
+    async verifyLoginButtonVisibiltyAndNavigation() {
+        await expect(this.productPage.loginButton).toBeVisible();
+        await this.productPage.loginButton.click();
+        await expect(this.productPage.page).toHaveURL(/login/);
+    }
+
+    async verifyRegisterButtonVisibiltyAndNavigation() {
+        await expect(this.productPage.registrationButton).toBeVisible();
+        await this.productPage.registrationButton.click();
+        await expect(this.productPage.page).toHaveURL(/register/);
+    }
+
+    async verifyHomePageBannerVisibilty() {
+        await expect(this.productPage.banner).toBeVisible();
+    }
+
+    async verifyBannerTitleVisibilty() {
+        await expect(this.productPage.bannerTitle).toBeVisible();
+    }
 }
--- tests/Qkart/productListing/productlisting.spec.ts
+++ tests/Qkart/productListing/productlisting.spec.ts
@@ -1,2 +1,14 @@
 import {test} from "../../../src/custom_fixture/fixture"
+import { ProductAction  } from "../../../src/actions/ProductListing/productAction"
 
+test("TC-001-Product Listing: Home Page Load and Navigation Verification", async ({page, appAction}) => {
+    await appAction.product.verifyPageLoadState();
+    await appAction.product.verifyLoginButtonVisibiltyAndNavigation();
+    await page.goBack();
+    await appAction.product.verifyRegisterButtonVisibiltyAndNavigation();
+})
+
+test("TC-003-Prodyct Listing: Banner Image and Title Visibilty", async ({appAction}) => {
+    await appAction.product.verifyHomePageBannerVisibilty();
+    await appAction.product.verifyBannerTitleVisibilty();
+})
