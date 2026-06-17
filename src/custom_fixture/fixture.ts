935b465453864716fbf0a681582905a8664acbda
Gona Vaishnavi <gona.vaishnavi@newvision-software.com>
2026-06-17 12:06:35 +0530

    Changes

--- src/custom_fixture/fixture.ts
+++ src/custom_fixture/fixture.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { test as base, expect, Page } from '@playwright/test';
-import {RegisterationAction} from "../actions/RegistrationAction/registrationAction";
-import RegisterationData from "../testdata/registrationData.json";
-import { CartAction } from '../actions/cartAction/cartAction';
-import loginData from '../testdata/login.json';
-import { loginAction } from '../actions/loginAction/loginAction';
-import { CheckoutAction } from "../actions/checkoutAction";
-=======
 import { test as base, expect } from "@playwright/test";
 import { loginAction } from "../actions/loginAction/loginAction";
 import loginData from "../testdata/login.json";
@@ -21,9 +12,6 @@ import { ProductAction } from '../actions/ProductListing/productAction';
 
 type AppActions = {
     login: loginAction;
-<<<<<<< HEAD
-    checkout: CheckoutAction;
-=======
     footer: FooterAction;
     register: RegistrationAction;
     cart: CartAction;
@@ -33,7 +21,6 @@ type AppActions = {
 
    
 
->>>>>>> 78868000af4b81d4b1c88f9e47c046a2018f3421
 };
 
 type Fixtures = {
@@ -56,12 +43,6 @@ export const test = base.extend<Fixtures>({
         await page.goto(loginData.BaseURL);
  
         const appAction: AppActions = {
-<<<<<<< HEAD
-            register: new RegisterationAction(page),
-            cart: new CartAction(page),
-            login: new loginAction(page),
-            checkout: new CheckoutAction(page)
-=======
             login: new loginAction(page),
             footer: new FooterAction(page),
             register: new RegistrationAction(page),
