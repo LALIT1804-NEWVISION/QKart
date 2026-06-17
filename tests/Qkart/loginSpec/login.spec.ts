a6a87fc33da2274528a7334b0bd97fabe1f1bd7b
Shubham Nigam <shubham.nigam@newvision-software.com>
2026-06-17 12:41:20 +0530

    updated

--- tests/Qkart/loginSpec/login.spec.ts
+++ tests/Qkart/loginSpec/login.spec.ts
@@ -4,7 +4,7 @@ import loginData from "../../../src/testdata/login.json";
 //test.use({ storageState: { cookies: [], origins: [] } });
 test("Login_01 Verify that a user can log in with a valid username and password", async({appAction}) => 
     {
-        // User Should login with valid credential and Validate the login successfully
+        
         console.log("Logging in with valid credentials", loginData.LoginDetails);
         await appAction.login.loginUser(loginData.LoginDetails);
         
@@ -13,7 +13,7 @@ test("Login_01 Verify that a user can log in with a valid username and password"
 
 test("Login_02 Verify login with an invalid username", async({appAction}) => 
     {
-        // User Should login with invalid username and Validate the error message
+        
         console.log("Attempting login with invalid username", loginData.incorrectLoginDetails);
         await appAction.login.loginWithInvalidUsername(loginData.incorrectLoginDetails);
         
@@ -22,34 +22,35 @@ test("Login_02 Verify login with an invalid username", async({appAction}) =>
 
 test("Login_03 Verify login with an invalid password", async({appAction}) => 
     {
-        // User Should login with invalid password and Validate the error message
+        
         console.log("Attempting login with invalid password", loginData.incorrectPassword);
         await appAction.login.loginWithInvalidPassword(loginData.incorrectPassword);
     }
 );
 
 test("Login_04 Verify login with a blank username", async ({appAction}) => {
-    // User Should login with blank username and Validate the error message
+    
     console.log("Attempting login with blank username", loginData.blankUsername);
     await appAction.login.loginWithBlankUsername(loginData.blankUsername);
 }
 );
 
 test("Login_05 Verify login with a blank password", async ({appAction}) => {
-    // User Should login with blank password and Validate the error message
+    
     console.log('Attempting login with blank password', loginData.blankPassword);
     await appAction.login.loginWithBlankPassword(loginData.blankPassword);
 }
 );
 
 test("Login_06 Verify logout functionality", async ({appAction}) => {
-    // User Should login with valid credential and Validate the login successfully
+    
     console.log("Logging in with valid credentials", loginData.LoginDetails);
     await appAction.login.logoutValidation(loginData.LoginDetails);
 })
 
 test("Login_07 Verify the login after refersh",async({appAction,page})=>{
-    //Verify that the user can log in successfully with valid credentials and remains logged in after refreshing the page 
+    // Verify that the user can log in successfully with valid credentials
+    // and remains logged in after refreshing the page 
      await appAction.login.loginUser(loginData.LoginDetails);
      await page.reload();
      await appAction.login.validogoutLogo();
@@ -57,7 +58,8 @@ test("Login_07 Verify the login after refersh",async({appAction,page})=>{
 })
 
 test("Login_08 Verify Product After Logout ",async({appAction})=>{
-    //Verify that the user can log in successfully with valid credentials and remains logged in after refreshing the page
+    // Verify that the user can log in successfully with valid 
+    // credentials and remains logged in after refreshing the page
      await appAction.login.verifyProductAfterLogout(loginData.AddtoCart.username,loginData.AddtoCart.password);
 })
 
